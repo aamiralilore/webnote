00737b99e5630162cf9ce3eb7181dee35dfcb381
aamiralilore <aamir.ali.lore@gmail.com>
2022-05-19 18:30:18 +0200

    Display welcome username using JS

--- static/script.js
+++ static/script.js
@@ -1,26 +1,28 @@
-const additem = document.getElementById("additem");
+const username = document.getElementById("username");
 const text = document.getElementById("text");
 const main = document.getElementById("main");
 const content = document.getElementById("content");
 
-function addingitem() {
-    const item = document.createElement("p");
-    item.innerText = text.value;
-    item.className = "beautiful";
-    content.appendChild(item);
-    const input = "item " + localStorage.length;
-    localStorage.setItem(input, text.value);
-}
+function addUsername() {
+        const name = document.createElement("p");
+        name.innerText = "Welcome " + text.value + " !";
+        name.className = "login";
+        content.appendChild(name);
+        const input = "name " + localStorage.length;
+        localStorage.setItem(input, text.value);
+    }
 
 function display() {
     for (let i= 0; i < localStorage.length; i++) {
-        const value = localStorage.getItem("item " + i);
-        const item = document.createElement("p");
-        item.innerText = value;
-        item.className= "beautiful";
-        content.appendChild(item);
+        const value = localStorage.getItem("name " + i);
+        const name = document.createElement("p");
+        name.innerText = "Welcome back " + value + " !";
+        name.className= "login";
+        content.appendChild(name);
     }
 }
 
-additem.addEventListener("Click", addingitem);
+
+
+username.addEventListener("click", addUsername);
 display();
